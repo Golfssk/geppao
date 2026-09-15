a4abb7a3e37035fe7f0c97e31edcb82248b71413
Golfssk <soravis.suk@gmail.com>
2026-09-15 23:23:20 +0700

    Add host login page

---
+++ app/host/login/page.tsx
@@ -0,0 +1,11 @@
+import { HostLoginForm } from '@/components/host/HostLoginForm';
+
+export default function HostLoginPage() {
+  return (
+    <main className="page">
+      <div className="container section">
+        <HostLoginForm />
+      </div>
+    </main>
+  );
+}
